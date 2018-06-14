bd77fb25e3441790459c96c688d3039f2a11ef29
danish123gupta <gupta.danish236@gmail.com>
2018-06-14 17:57:12 +0530

    assignment is complete

---
+++ assignment 3.py
@@ -0,0 +1,35 @@
+#Q.NO.1
+"""a=input("enter the value ")
+l=[]
+l.append(a)
+print(l)
+#Q.no.2
+m=['google",apple",facebook",microsoft",tesla']
+n=l+m
+print(n)"""
+
+#q.no.3
+a=["apple","mango","apple"]
+print(a.count("apple"))
+
+#q.no.4
+b=['a','d','c','e','b']
+b.sort()
+print (b)
+
+#Q.no.5
+a=[1,3,7]
+b=[6,8,9]
+c= a+b
+c.sort()
+print(c)
+
+#Q.NO.6
+l=["shivangi","megha","nisha"]
+l.append("ayush")
+l.append("shukal")
+print(l)
+print(l.pop())
+
+
+
